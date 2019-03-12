aecaa199cf4671e49316b186c20d831c7f7bf562
Yudin Nikita <yudin.nc@gmail.com>
2019-03-12 23:47:24 +0500

    ver3

--- app/js/common.js
+++ app/js/common.js
@@ -3,12 +3,48 @@ $(function() {
 	$(".slider1__container").slick({
 		centerMode: true,
 		centerPadding: '60px',
-		slidesToShow: 5
+		slidesToShow: 5,
+
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 	});
 	$(".slider2__container").slick({
 		fade: true,
 		speed: 700,
 		adaptiveHeight: true,
 		infinite: false
+	});
+	$(".days__container").slick({
+		fade: true,
+		speed: 700,
+		infinite: false,
+		adaptiveHeight: true,
+	  	responsive: [
+		    {
+		      breakpoint: 1920,
+		       settings: "unslick"
+		    },
+		    {
+		      breakpoint: 992,
+		       settings: "slick"
+		    }
+		    ]
 	});
 });
